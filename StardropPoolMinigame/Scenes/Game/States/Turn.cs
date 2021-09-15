d7f37188215304d58b5f1a7382d477bbcfd6c78f
Andrew Young <andrew@youngshome.com>
2021-09-15 12:01:56 -0700

    Turn changes

--- StardropPoolMinigame/Scenes/Game/States/Turn.cs
+++ StardropPoolMinigame/Scenes/Game/States/Turn.cs
@@ -6,15 +6,43 @@ namespace StardropPoolMinigame.Scenes.States
 {
     class Turn
     {
+        /// <summary>
+        /// <see cref="TurnResults"/> of <see cref="Turn"/>.
+        /// </summary>
+        private TurnResults _results;
+
+        /// <summary>
+        /// <see cref="IList"/> of <see cref="IPlayer"/>.
+        /// </summary>
         private IList<IPlayer> _players;
 
+        /// <summary>
+        /// Index of <see cref="IPlayer"/> whose <see cref="Turn"/> it is.
+        /// </summary>
         private int _playerIndex;
 
+        /// <summary>
+        /// Current <see cref="TurnState"/>.
+        /// </summary>
         private TurnState _turnState;
 
+        /// <summary>
+        /// Whether the last <see cref="Turn"/> was a scratch and a new <see cref="Ball"/> needs to be placed.
+        /// </summary>
         private bool _needsToPlaceCueBall;
 
-        public Turn(IList<IPlayer> players, int playerIndex, bool wasScratch = false, bool selectPocket = false)
+        /// <summary>
+        /// Instantiates a new <see cref="Turn"/>.
+        /// </summary>
+        /// <param name="players"><see cref="IList"/> of <see cref="IPlayer"/></param>
+        /// <param name="playerIndex">Index of <see cref="IPlayer"/> whose <see cref="Turn"/> it is</param>
+        /// <param name="wasScratch">Whether the last <see cref="Turn"/> was a scratch and a new <see cref="Ball"/> needs to be placed</param>
+        /// <param name="selectPocket">Whether the next <see cref="Ball"/> requires a pocket to be specified</param>
+        public Turn(
+            IList<IPlayer> players, 
+            int playerIndex = Math.Round(MiscellaneousHelper.Random()),
+            bool wasScratch = false, 
+            bool selectPocket = false)
         {
             this._players = players;
             this._playerIndex = playerIndex;
