f3358b1300fc61435183c32600884af66a0bc090
Andrew Young <andrew@youngshome.com>
2025-02-09 01:56:15 -0800

    Entities

---
+++ MinigameFramework/Render/Filters/Filter.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MinigameFramework.Render.Filters
+{
+    /// <inheritdoc cref="IFilter"/>
+    abstract class Filter : IFilter
+    {
+        /// <summary>
+        /// Filter unique ID.
+        /// </summary>
+        protected string _key;
+
+        /// <summary>
+        /// Instantiates a new filter.
+        /// </summary>
+        public Filter()
+        {
+            _key = null;
+        }
+
+        /// <summary>
+        /// Instantiates a new filter.
+        /// </summary>
+        /// <param name="key">Forces a filter key value.</param>
+        public Filter(string key)
+        {
+            SetKey(key);
+        }
+
+        /// <inheritdoc cref="IFilter.GetColor"/>
+        public virtual Color GetColor(Color color)
+        {
+            return color;
+        }
+
+        /// <inheritdoc cref="IFilter.GetDestination"/>
+        public virtual Vector2 GetDestination(Vector2 destination)
+        {
+            return destination;
+        }
+
+        /// <inheritdoc cref="IFilter.GetEffects"/>
+        public virtual SpriteEffects GetEffects(SpriteEffects effects)
+        {
+            return effects;
+        }
+
+        /// <inheritdoc cref="IFilter.GetLayerDepth"/>
+        public virtual float GetLayerDepth(float layerDepth)
+        {
+            return layerDepth;
+        }
+
+        /// <inheritdoc cref="IFilter.GetOrigin"/>
+        public virtual Vector2 GetOrigin(Vector2 origin)
+        {
+            return origin;
+        }
+
+        /// <inheritdoc cref="IFilter.GetRotation"/>
+        public virtual float GetRotation(float rotation)
+        {
+            return rotation;
+        }
+
+        /// <inheritdoc cref="IFilter.GetScale"/>
+        public virtual float GetScale(float scale)
+        {
+            return scale;
+        }
+
+        /// <inheritdoc cref="IFilter.GetSource"/>
+        public virtual Rectangle GetSource(Rectangle source)
+        {
+            return source;
+        }
+
+        /// <inheritdoc cref="IFilter.GetName"/>
+        public abstract string GetName();
+
+        /// <inheritdoc cref="IFilter.SetKey"/>
+        public virtual void SetKey(string key)
+        {
+            _key = $"{key}-filter-{GetName()}";
+        }
+
+        /// <inheritdoc cref="IFilter.Start"/>
+        public virtual void Start()
+        {
+            Utilities.Timer.StartTimer(_key);
+        }
+
+        /// <inheritdoc cref="IFilter.Stop"/>
+        public virtual void Stop()
+        {
+            Utilities.Timer.EndTimer(_key);
+        }
+
+        /// <summary>
+        /// Returns the duration of the filter passed.
+        /// </summary>
+        protected float GetTimePassed()
+        {
+            if (_key != null)
+            {
+                return Utilities.Timer.CheckTimer(_key);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ MinigameFramework/Render/Filters/Transitions/ScaleUp.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Xna.Framework;
+using MinigameFramework.Enums;
+
+namespace MinigameFramework.Render.Filters.Transitions
+{
+    /// <summary>
+    /// Entity starts small and scales to size.
+    /// </summary>
+    class ScaleUp : Transition
+    {
+        /// <summary>
+        /// How small the sprite should start.
+        /// </summary>
+        protected float _startingScale;
+
+        /// <summary>
+        /// Instantiates a scaling up transition.
+        /// </summary>
+        /// <param name="duration">Duration of the transition.</param>
+        /// <param name="keyframeOpacity">Whether to keyframe the opacity as it scales.</param>
+        /// <param name="type"><see cref="TransitionState"/> of transition.</param>
+        /// <param name="startingScale">How small the sprite should start.</param>
+        public ScaleUp(
+            int duration,
+            bool keyframeOpacity,
+            TransitionState type,
+            float startingScale = 0
+        ) : base(
+            duration,
+            keyframeOpacity,
+            type
+        ) {
+            this._startingScale = startingScale;
+        }
+
+        /// <summary>
+        /// Instantiates a scaling up transition.
+        /// </summary>
+        /// <param name="duration">Duration of the transition.</param>
+        /// <param name="keyframeOpacity">Whether to keyframe the opacity as it scales.</param>
+        /// <param name="type"><see cref="TransitionState"/> of transition.</param>
+        /// <param name="delay">Delay the transition.</param>
+        /// <param name="startingScale">How small the sprite should start.</param>
+        public ScaleUp(
+            int duration,
+            bool keyframeOpacity,
+            TransitionState type,
+            int delay,
+            float startingScale = 0
+        ) : base(
+            duration,
+            keyframeOpacity,
+            type,
+            delay
+        ) {
+            this._startingScale = startingScale;
+        }
+
+        /// <inheritdoc cref="IFilter.GetColor"/>
+        public override Color GetColor(Color color)
+        {
+            if (this._keyframeOpacity)
+            {
+                float progress = this._type == TransitionState.Entering ? this.GetProgress() : this.GetInvertedProgress();
+
+                return new Color(
+                    (byte)Math.Round(progress * color.R),
+                    (byte)Math.Round(progress * color.G),
+                    (byte)Math.Round(progress * color.B),
+                    (byte)Math.Round(progress * 255));
+            }
+            return color;
+        }
+
+        /// <inheritdoc cref="IFilter.GetScale"/>
+        public override float GetScale(float scale)
+        {
+            float progress = this._type == TransitionState.Entering ? this.GetProgress() : this.GetInvertedProgress();
+
+            return this._startingScale + ((1 - this._startingScale) * progress) + 0.001f;
+        }
+
+        /// <inheritdoc cref="IFilter.GetName"/>
+        public override string GetName()
+        {
+            return "scale-up";
+        }
+    }
+}
